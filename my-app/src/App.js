ede0b02b0a5e1b0b0c8caeec542c56d460da73a4
Julian Leal <julianleal1997@gmail.com>
2022-07-11 13:50:27 -0500

    fingers crossed

---
+++ my-app/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import React, { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import Matches from "./components/Matches.js"
+import Bio from "./components/Bio.js"
+import NavBar from "./components/NavBar.js"
+
+function App() {
+
+  const userURL = "http://localhost:9292/users"
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    fetch(userURL)
+      .then((response) => response.json())
+      .then((data) => { 
+        //console.log(data[0])
+        setCurrentUser(data[0])
+        }) 
+  }, []);
+
+    return (
+        <div>
+        <NavBar />
+        <Switch>
+            <Route exact path="/">
+                <Bio currentUser ={currentUser}
+                 />
+            </Route>
+            <Route path="/matches">
+                <Matches currentUser ={currentUser}/>
+            </Route>
+        </Switch>
+    </div>
+    )
+}
+
+export default App;
+
+
+/* <Route path="/swiper" currentUser ={currentUser}>
+<Swiper 
+/>
+</Route> */
